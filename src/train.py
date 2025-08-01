f3261913336b6982b33b07462c4bc3dc69e59660
paperspace <qinglinh@usc.edu>
2025-08-01 22:13:34 +0000

    refactor(train): inject configuration parameters from YAML

--- src/train.py
+++ src/train.py
@@ -2,25 +2,27 @@ from transformers import Seq2SeqTrainingArguments as TrainingArguments, Trainer
 from data_loader import load_dataset_from_json
 from utils import get_tokenizer, load_model, get_data_collator, tokenize_batch
 
-def main():
-    model_name = "google/flan-t5-small"
-    data_path = "data/polite_pairs.json"
+def main(config):
+    model_name = config["model"]["name_or_path"]
+    data_path = config["data"]["train_path"]
+    val_ratio = config["data"]["val_ratio"]
+    max_length = config["data"]["max_length"]
 
-    datasets = load_dataset_from_json(data_path, val_ratio=0.1)
+    datasets = load_dataset_from_json(data_path, val_ratio)
     tokenizer = get_tokenizer(model_name)
-    datasets = datasets.map(lambda x: tokenize_batch(tokenizer, x), batched=True, remove_columns=["input", "target"])
+    datasets = datasets.map(lambda x: tokenize_batch(tokenizer, x, max_length), batched=True, remove_columns=["input", "target"])
+
     model = load_model(model_name)
     data_collator = get_data_collator(tokenizer, model)
 
     training_args = TrainingArguments(
-        output_dir="model/polite-rewriter",
-        num_train_epochs=8,
-        per_device_train_batch_size=8,
-        learning_rate=3e-5,
-        logging_steps=50,
-        save_total_limit=2,
-        predict_with_generate=True,
-        fp16=True
+        output_dir=config["model"]["save_dir"],
+        num_train_epochs=config["training"]["epochs"],
+        per_device_train_batch_size=config["training"]["batch_size"],
+        learning_rate=config["training"]["learning_rate"],
+        logging_steps=config["training"]["logging_steps"],
+        save_total_limit=config["training"]["save_total_limit"],
+        fp16=config["training"]["fp16"]
     )
 
     trainer = Trainer(
@@ -31,13 +33,12 @@ def main():
         tokenizer=tokenizer,
         data_collator=data_collator
     )
-
     trainer.train()
 
-    model.save_pretrained("model/polite-rewriter")
-    tokenizer.save_pretrained("model/polite-rewriter")
+    model.save_pretrained(config["model"]["save_dir"])
+    tokenizer.save_pretrained(config["model"]["save_dir"])
 
 if __name__ == "__main__":
-    main()
+    raise ValueError("Please run this script via cli.py using --mode train")
 
 
